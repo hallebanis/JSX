af47099957e571691b847c4efb459f78ae8da298
Anis Halleb <halleb.anis6@gmail.com>
2020-10-09 23:16:10 +0200

    JSX Checkpoint

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import videoPath from "./Video/video.mp4"
 import "./myApp.css"
 
 function App() {
+
+  const imgStyle={
+      display:"block",
+      width:600,
+      marginBottom:"10px"
+  };
   return (
     <div className="App">
       <header className="header-main"></header>
       <main className="App-main">
       <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
       <h1 className="title red">HALLEB ANIS</h1>
-      <img src={firstPic} alt="" style={{width:600,display:"block",marginBottom:"10px"}} />
-      <img src={secondPic} alt="" style={{width:600,display:"block",marginBottom:"10px"}}/>
+      <img src={firstPic} alt="" style={imgStyle} />
+      <img src={secondPic} alt="" style={imgStyle}/>
       <video style={{width:"320", height:"240"}} controls>
         <source src={videoPath} type="video/mp4" />
       </video>
